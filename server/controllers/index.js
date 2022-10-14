c7fdd28c50589c365e447d22eb1df1178c626581
Vincent Li (RIT Student) <vl5604@rit.edu>
2022-10-14 13:50:25 -0400

    finished

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,35 @@
+const fs = require('fs');
+const movies = JSON.parse(fs.readFileSync(`${__dirname}/../../data/movies.json`));
+
+
+
 const hostIndex = (req, res) => {
   res.render('index');
 };
+
+const getData = (req, res) => {
+  res.json(movies.filter(movie => movie.title === 'Gone with the Wind'));
+}
+
+const hostResults = (req, res) => {
+  let results = movies;
+
+  if (req.query.title) {
+    results = results.filter(movie => movie.title.includes(req.query.title));
+  }
+
+  if (req.query.year) {
+    const year = parseInt(req.query.year, 10);
+    results = results.filter(movie => movie.year === year);
+  }
+
+  if (req.query.starring) {
+    results = results.filter(movie => movie.cast.includes(req.query.starring));
+  }
+
+  // console.log(req.query);
+  res.render('results', { movies: results , search: req.query});
+}
 
 const notFound = (req, res) => {
   res.status(404).render('notFound', {
@@ -11,4 +40,6 @@ const notFound = (req, res) => {
 module.exports = {
   index: hostIndex,
   notFound,
+  getData,
+  results: hostResults,
 };
--- server/router.js
+++ server/router.js
@@ -3,6 +3,8 @@ const controllers = require('./controllers');
 const router = (app) => {
   app.get('/', controllers.index);
 
+  app.get('/getData', controllers.getData);
+  app.get('/results', controllers.results);
   app.get('/*', controllers.notFound);
 };
 
